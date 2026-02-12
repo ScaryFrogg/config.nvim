50137fcb70c8f71b1dcee654b4a8ea01a8f80ed1
Vojin Puric <puricvojin@gmail.com>
2026-02-12 16:28:51 +0100

    tmux navigator

--- init.lua
+++ init.lua
@@ -264,7 +264,6 @@ require("lazy").setup({
   { -- Fuzzy Finder (files, lsp, etc)
     "nvim-telescope/telescope.nvim",
     event = "VimEnter",
-    branch = "0.1.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
@@ -787,7 +786,7 @@ require("lazy").setup({
       -- Prefer git instead of curl in order to improve connectivity in some environments
       require("nvim-treesitter.install").prefer_git = true
       ---@diagnostic disable-next-line: missing-fields
-      require("nvim-treesitter.configs").setup(opts)
+      require("nvim-treesitter.config").setup(opts)
 
       -- There are additional nvim-treesitter modules that you can use to interact
       -- with nvim-treesitter. You should go explore a few and see what interests you:
@@ -811,6 +810,7 @@ require("lazy").setup({
   require "custom.plugins.http",
   require "custom.plugins.snacks",
   require "custom.plugins.markdown",
+  require "custom.plugins.tmux-navigator",
   --Windows machien plugins
   --require "custom.plugins.omnisharp",
 
